612a7bed5a4f1ab505cf43865112e6095595637b
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2023-01-17 18:57:46 -0500

    Adding sections to website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import About from './components/About';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
 
   return (
     <div>
       <main>
-        <About></About>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
       </main>
     </div>
   );
